5a9e11fa4e6f538578534680d7c80f9e0abf1d60
muhammadrezki <iskarimrezki@gmail.com>
2020-09-23 21:50:48 +0700

    add Lab 2 source file

---
+++ app/src/main/java/com/example/MobileProgramming/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.MobileProgramming;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int count = 0;
+    private TextView numberText;
+    private Button increment;
+    private Button decrement;
+    private int counter;
+
+    private View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.incrementBtn:
+                    increment();
+                    break;
+                case R.id.decrementBtn:
+                    decrement();
+                    break;
+            }
+
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        numberText = (TextView)findViewById(R.id.numberText);
+        increment = (Button)findViewById(R.id.incrementBtn);
+        increment.setOnClickListener(clickListener);
+        decrement = (Button)findViewById(R.id.decrementBtn);
+        decrement.setOnClickListener(clickListener);
+
+
+    }
+
+    private void initCounter(){
+        counter = 0;
+        numberText.setText(counter);
+    }
+
+
+    public void increment (){
+        count++;
+        numberText.setText(count + "");
+    }
+
+    public void decrement () {
+        if (count <= 0) count = 0;
+        else
+            count--;
+            numberText.setText(count + "");
+    }
+}
